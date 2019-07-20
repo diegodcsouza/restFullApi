efadb66cd74ce305295aa3cfab6babe1c2501621
Diego Costa <diegodcsouza@gmail.com>
2019-07-19 21:04:48 -0300

    DataContract

--- restFullApi 02 - Using Diferent Verbs/restFullApi/Data/VO/BookVO.cs
+++ restFullApi 02 - Using Diferent Verbs/restFullApi/Data/VO/BookVO.cs
@@ -1,17 +1,25 @@
 ﻿using System;
+using System.Runtime.Serialization;
 
 namespace restFullApi.Data.VO
 {
+    [DataContract]
     public class BookVO
     {
+        //DataContract é responsavel por serializar os objetos.
+        [DataMember (Order = 1, Name ="Código")]
         public long? Id { get; set; }
 
+        [DataMember(Order = 2, Name = "Título")]
         public string Title { get; set; }
 
+        [DataMember(Order = 3, Name = "Autor")]
         public string Author { get; set; }
 
+        [DataMember(Order = 5, Name = "Preço")]
         public decimal Price { get; set; }
 
+        [DataMember(Order = 4, Name = "Lançamento")]
         public DateTime LaunchDate { get; set; }
     }
 }
